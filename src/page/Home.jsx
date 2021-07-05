442e1356edd89b8072f02cc6bd16f0d4cd9d5963
Park cloud <340688748@qq.com>
2021-07-05 13:23:50 +0800

    hero section IpadAir

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import AppHeader from '../component/Appbar';
 import ProductNavBar from '../component/ProductNavBar';
 import Ribbon from '../component/Ribbon';
-import {Grid} from '@material-ui/core'
+import { Grid } from '@material-ui/core';
 import IPadProHero from '../section/hero/IPadPro';
+import IPadAirHero from '../section/hero/IPadAir';
 
 const Home = () => {
 	return (
 		<React.Fragment>
 			<AppHeader />
-		  <ProductNavBar/>
-      <Grid component='main' container sx={{width:'100%'}}>
-        <Ribbon/>
-        <IPadProHero/>
-      </Grid>
+			<ProductNavBar />
+			<Grid component='main' container sx={{ width: '100%' }}>
+				<Ribbon />
+				<IPadProHero />
+				<IPadAirHero />
+			</Grid>
 		</React.Fragment>
 	);
 };
